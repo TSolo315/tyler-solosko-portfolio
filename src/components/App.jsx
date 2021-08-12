40e5e38c1c2ad0057df545f2c80d984f7e6c1c06
Tyler Solosko <TSolo215@live.com>
2021-08-12 17:46:36 -0600

    Added contact modal skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
 import Hero from './Hero/Hero';
 import About from './About/About';
 import Projects from './Projects/Projects';
@@ -9,11 +10,14 @@ import { PortfolioProvider } from '../context/context';
 
 import { aboutData, projectsData, contactData, footerData } from '../mock/data';
 
+Modal.setAppElement('#___gatsby');
+
 function App() {
   const [about, setAbout] = useState({});
   const [projects, setProjects] = useState([]);
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     setAbout({ ...aboutData });
@@ -22,12 +26,43 @@ function App() {
     setFooter({ ...footerData });
   }, []);
 
+  // function afterOpenModal() {
+  //   subtitle.style.color = '#f00';
+  // }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const customStyles = {
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      // backgroundColor: 'rgba(255, 255, 255, 0.75)',
+    },
+  };
+
   return (
     <PortfolioProvider value={{ about, projects, contact, footer }}>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Contact Modal"
+      >
+        asdasdasd
+      </Modal>
       <Hero />
       <About />
       <Projects />
-      <Contact />
+      <Contact setModalOpen={setIsOpen} />
       <Footer />
     </PortfolioProvider>
   );
